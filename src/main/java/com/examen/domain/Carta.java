dc626fca637c44f4edcadc031579ef56b71d1e7a
Juan Senen <juansenen@hotmail.com>
2022-05-06 18:30:41 +0200

    Subida clase Carta

---
+++ src/main/java/com/examen/domain/Carta.java
@@ -0,0 +1,17 @@
+package com.examen.domain;
+
+public class Carta {
+    private String palo;
+    private String numero;
+
+    public Carta(String palo, String numero) {
+        this.palo = palo;
+        this.numero = numero;
+    }
+
+    public int getValor(){
+
+        return 0;
+    }
+
+}
